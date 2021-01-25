23dd7d49ac108b39f079ea9aa50e1801c9edba8f
Hankscr459 <defcw58@gmail.com>
2021-01-25 15:28:40 +0800

    More Props & State Work

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,13 +2,18 @@ import React, { FC } from 'react'
 
 interface TodoListProps {
     items: {id: string, text: string}[]
+    onDeleteTodo: (id: string) => void
 }
+// onClick={() => props.onDeleteTodo(todo.id)}
 const TodoList: FC<TodoListProps> = (props) => {
     
     return (
         <ul>
             {props.items.map(todo => 
-                <li key={todo.id}>{todo.text}</li>
+                <li key={todo.id}>
+                    <span>{todo.text}</span>
+                    <button onClick={props.onDeleteTodo.bind(null, todo.id)}>DELETE</button>
+                </li>
             )}
         </ul>
     )
